104d0f6084fb2f9a057476bc04d658cbf27ddbed
Dulat Khojabergen <khojabergen@gmail.com>
2021-12-21 17:06:30 +0600

    pre-setup

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.theme')
 @section('content')
 
-    <h1>hello world...</h1>
+    <section id="hero">
+        <h1>Hello world...</h1>
+        <a data-remodal-target="testmodal" href="#">Call Modal</a>
+    </section>
 
 @endsection
---
+++ resources/views/partials/modals.blade.php
@@ -0,0 +1,11 @@
+<div class="remodal" data-remodal-id="testmodal" data-remodal-options="hashTracking: false, closeOnOutsideClick: false">
+
+    <button data-remodal-action="close" class="remodal-close"></button>
+    <h1>Test modal</h1>
+    <p>
+        This is the test modal. Responsive, lightweight, fast, synchronized with CSS animations, fully customizable modal window plugin with declarative configuration and hash tracking.
+    </p>
+    <br>
+    <button data-remodal-action="cancel" class="remodal-cancel">Cancel</button>
+    <button data-remodal-action="confirm" class="remodal-confirm">OK</button>
+</div>
