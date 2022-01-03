f7d56458fb6e347905d41b3f2e2f202247c423c8
Pinksky <lipn5757@gmail.com>
2022-01-03 13:52:36 +0500

    Added: new_location modal

--- resources/views/partials/modals.blade.php
+++ resources/views/partials/modals.blade.php
@@ -16,5 +16,27 @@
     <div class="new_location">
         <h3>Создайте новый адрес</h3>
         <p>Вы всегда можете редактировать данные адресов</p>
+        <form method="POST" action="#" class="location-form">
+            <input type="text" name="place" placeholder="Введите название адреса ( Дом, Работа... )" class="place"/>
+            <fieldset>
+              <select>
+                <option selected>Республика Казахстан</option>
+              </select>
+            </fieldset>
+            <fieldset>
+              <legend>Область, город</legend>
+              <input type="tel" name="city" value="+7 (718) 878 38 28"/>
+            </fieldset>
+            <fieldset>
+              <legend>Адрес</legend>
+              <input type="text" name="address" value="Сулейменова"/>
+            </fieldset>
+            <fieldset>
+              <legend>Комментарий</legend>
+              <textarea></textarea>
+            </fieldset>
+            <button type="submit" class="main-btn">Сохранить адрес</button>
+            <button data-remodal-action="cancel" class="secondary-btn">Отменить</button>
+        </form>
     </div>
 </div>
